aba6f4b2417983ff73946f70cf0fe561f3565175
Brandon Gutzmann <bgutz001@ucr.edu>
2017-03-05 17:24:06 -0800

    Completed leav-one-out validation
    Completed forward selection
    
    Completed normalizing input data
    
    Implemented backwards elimination but it is wonky

--- main.cpp
+++ main.cpp
@@ -33,16 +33,36 @@ struct Object {
 double distance
 (const std::vector<int> &featureIndicies, const Object &obj1, const Object &obj2);
 
-// Returns the class of the nearest neighbor.
+// Returns the index of the nearest neighbor.
 // featureIndicies is the idindices of the features to use in the distance formula.
 // objects should be a vector of all objects.
 // index should be the index of the object in objects that you want to find the nearest neighbor for.
 int nearestNeighbor
 (const std::vector<int> &featureIndicies, const std::vector<Object> &objects, const int index);
 
+// Normalize features of objetcs
+// All object->features should be of the same length
+// objects is the list of objects whose features you want to normalize
+void normalize
+(std::vector<Object> &objects);
+
+// Leave one out validation
+// Returns the accuracy of using certain features
+// featureIndicies is a list of indicies that indecies in object->features
+// objects is a list of Objects that we are finding the accuracy of
+double validation
+(const std::vector<int> &featureIndicies, const std::vector<Object> &objects);
+
 // Greedy forward selection algorithm
 // Add one feature at a time picking the best one to expand upon
-void forwardSelection(std::vector<int> &featureList);
+std::vector<int> forwardSelection
+(std::vector<Object> &trainingSet);
+
+// Greedy backward elimination algorithm
+// Start with a classification using all the features
+// Get rid of features one by one until they start to decrease accuracy
+std::vector<int> backwardElimination
+(std::vector<Object> &trainingSet);
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -51,6 +71,8 @@ int main(int argc, char* argv[]) {
     exit(-1);
   }
 
+  std::vector<Object> instances;
+
   std::fstream file;
   file.open(argv[1]);
   if (file.fail()) {
@@ -58,7 +80,6 @@ int main(int argc, char* argv[]) {
     exit(-2);
   }
 
-  std::vector<Object> instances;
   std::string line;
   double temp;
   while (!getline(file, line).eof()) {
@@ -69,7 +90,6 @@ int main(int argc, char* argv[]) {
       // Delete leading whitespace
       while (line.at(0) == ' ') {
 	line.erase(0, 1);
-	std::cout << line << std::endl;
       }
       
       instances.back().features.push_back(temp);
@@ -81,16 +101,31 @@ int main(int argc, char* argv[]) {
     }
   }
 
+  file.close();
+  std::cout << "Done reading input data" << std::endl;
+  
+  normalize(instances);
+  std::cout << "Done normalizing input data" << std::endl;
 
+  /*
   // DEBUG
   std::cout << "num instances: " << instances.size() << std::endl;
   for (auto it = instances.begin(); it != instances.end(); ++it) {
     it->print();
   }
-  
-  file.close();
-  
-  
+  // * DEBUG
+  */
+
+  std::vector<int> featureList = backwardElimination(instances);
+
+
+  // OUTPUT
+  std::cout << "Feature list {";
+  for (int i = 0; i < featureList.size()-1; ++i) {
+    std::cout << featureList.at(i) << ", ";
+  }
+  std::cout << featureList.back() << "} is the best feature subset, with an accuracy of "
+	    << validation(featureList, instances) << std::endl;
   
   return 0;
 }
@@ -107,7 +142,7 @@ double distance
 
 int nearestNeighbor
 (const std::vector<int> &featureIndicies, const std::vector<Object> &objects, const int index) {
-  assert(index > objects.size());
+  assert(index < objects.size());
 
   double minDistance = 1.0/0.0;  // Set min distance to INF
   double curDistance = 0;
@@ -126,29 +161,146 @@ int nearestNeighbor
       minIndex = i;
     }
   }
+
+  return minIndex;
 }
 
+void normalize
+(std::vector<Object> &objects) {
+  // Check if there are features to normalize
+  if (objects.at(0).features.size() <= 1) {
+    return;
+  }
+
+  double max = 0;
+  // Go through features but skip index 0 (class)
+  for (int i = 1; i < objects.at(0).features.size(); ++i) {
+    // Go through objects and find max value of feature
+    for (int j = 0; j < objects.size(); ++j) {
+      if (std::abs(objects.at(j).features.at(i)) > max) {
+	max = std::abs(objects.at(j).features.at(i));
+      }
+    }
+  }
+
+  // Normalize
+  for (int i = 1; i < objects.at(0).features.size(); ++i) {
+    for (int j = 0; j < objects.size(); ++j) {
+      objects.at(j).features.at(i) /= max;
+    }
+  }
+
+}
+
+double validation
+(const std::vector<int> &featureIndicies, const std::vector<Object> &objects) {
+  double correct = 0; // Number of correct classifications
+  // Go through objects leaving one out each time
+  for (int i = 0; i < objects.size(); ++i) {
+    if (objects.at(nearestNeighbor(featureIndicies, objects, i)).features.at(0)
+	== objects.at(i).features.at(0)) {
+      ++correct;
+    }
+  }
+
+  return correct / objects.size();
+}
+
+
 std::vector<int>
-forwardSelection(std::vector<Object> &trainingSet, int cnt) {
-  static std::vector<int> featureList;
+forwardSelection(std::vector<Object> &trainingSet) {
   if (trainingSet.size() == 0) {
-    return featureList;
+    std::cout << "Error: Training set has no objects" << std::endl;
+    exit(0);
   }
+
+  std::vector<int> featureList;
+  double accuracy = 0;
+  int feature = 1;
+  bool gain = true; // If we are still gaining accuracy
+
+  while(gain) {
+    gain = false;
+    for (int i = 1; i < trainingSet.at(0).features.size(); ++i) {
+      // Check if i is already in features list
+      bool exists = true;
+      while (exists) {
+	exists = false;
+	for (auto it = featureList.begin(); it != featureList.end(); ++it) {
+	  if (i == *it) {
+	    exists = true;
+	    ++i;
+	    break;
+	  }
+	}
+      }
+      
+      featureList.push_back(i);
+      double curAccuracy = validation(featureList, trainingSet);
+      std::cout << "Acurracy with features: ";
+      for (auto temp = featureList.begin(); temp != featureList.end(); ++temp) {
+	std::cout << *temp << ' ';
+      }
+      
+      std::cout << "is: " << curAccuracy << std::endl;
+      if (curAccuracy > accuracy) {
+	accuracy = curAccuracy;
+	gain = true;
+	feature = i;
+      }
+      featureList.pop_back();
+    }
+    if (gain) {
+      std::cout << "Best choice is feature: " << feature << std::endl;
+      featureList.push_back(feature);
+    }
+  } 
   
-  static int numFeatures = trainingSet.at(0).features.size();
+  return featureList;
+}
 
-  // TODO: fix condition
-  for (int i = 0 ; i < numFeatures - cnt; ++i) {
-    // Add feature
-    featureList.push_back(i);
-    // Check its accuracy
-
-    // If it has the best accuracy so far keep it
-
-    // If not then remove it
-    //    featureList.pop();
+std::vector<int> backwardElimination
+(std::vector<Object> &trainingSet) {
+  if (trainingSet.size() == 0) {
+    std::cout << "Error: Training set has no objects" << std::endl;
+    exit(0);
   }
 
-  //forwardSelection(std::vector<Object> trainingSet, cnt + 1);
+  // Fill feature list with every feature
+  std::vector<int> featureList;
+  for (int i = 1; i < trainingSet.at(0).features.size(); ++i) {
+    featureList.push_back(i);
+  }
+  double accuracy = 0;
+  bool gain = true;
+
+  while (gain) {
+    gain = false;
+
+    int featureIndex;
+    for (int i = 0; i < featureList.size(); ++i) {
+      std::vector<int> temp;
+      // Copy all features but one
+      for (int j = 0; j < featureList.size(); ++j) {
+	if (i == j)
+	  continue;
+	temp.push_back(featureList.at(j));
+      }
+      double curAccuracy = validation(temp, trainingSet);
+      std::cout << "curAccuracy " << curAccuracy << std::endl;
+      if (curAccuracy > accuracy) {
+	gain = true;
+	accuracy = curAccuracy;
+	featureIndex = i;
+      }
+    }
+    if (gain) {
+      std::cout << "Best choice is feature: " << featureList.at(featureIndex) 
+		<< " accuracy " << accuracy << std::endl;
+      std::swap(featureList.at(featureIndex), featureList.back());
+      featureList.pop_back();
+    }
+  }
+
   return featureList;
 }
